c61085c795645b3e0f0fa3a95b316f1d75141676
vladimirbabin <vladimir.sol.rojo@gmail.com>
2025-10-12 20:05:21 +0300

    Initial commit

---
+++ src/main/java/com/vladimirbabin/github/async_spring_practice/AsyncSpringPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.vladimirbabin.github.async_spring_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncSpringPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncSpringPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vladimirbabin/github/async_spring_practice/AsyncSpringPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vladimirbabin.github.async_spring_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncSpringPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
